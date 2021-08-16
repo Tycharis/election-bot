73fec4f576cabfe1e90d26da2ad6d87d26f2ac5e
Braedon Smith <11038206+Tycharis@users.noreply.github.com>
2021-08-16 08:49:28 -0500

    Update Meta.java

--- src/main/java/edu/kstate/electionbot/Meta.java
+++ src/main/java/edu/kstate/electionbot/Meta.java
@@ -4,5 +4,5 @@ public final class Meta {
     public static final String NAME = "Election Bot";
     public static final String AUTHOR = "Braedon Smith";
     public static final String VERSION = "1.0-SNAPSHOT";
-    public static final String REPO_URL = "https://github.com/smithb99/election-bot";
+    public static final String REPO_URL = "https://github.com/Tycharis/election-bot";
 }
